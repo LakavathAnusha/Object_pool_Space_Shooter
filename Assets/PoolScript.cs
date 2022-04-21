ac4c35e35409099975d46c6e5c0674c0a9202b4d
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-04-21 16:31:38 +0530

    Bullet is added to the pool

--- Assets/PoolScript.cs
+++ Assets/PoolScript.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class PoolScript : MonoBehaviour
 {
+    // Start is called before the first frame update
     public static PoolScript instance;
     public List<GameObject> pool = new List<GameObject>();
     public List<PoolObject> poolItems = new List<PoolObject>();
     private void Awake()
     {
-        if(instance==null)
+        if (instance == null)
         {
             instance = this;
         }
         return;
     }
-    // Start is called before the first frame update
     void Start()
     {
         AddToPool();
@@ -24,36 +24,57 @@ public class PoolScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+
     }
+
     public void AddToPool()
     {
-        foreach(PoolObject item in poolItems)
+        foreach (PoolObject item in poolItems)
         {
-            for(int i=0;i<item.amount;i++)
+            for (int i = 0; i < item.amount; i++)
             {
-               GameObject temp= Instantiate(item.gameObjectPrefab);
+                GameObject temp = Instantiate(item.prefab);
                 temp.SetActive(false);
                 pool.Add(temp);
+
             }
         }
     }
-    public GameObject GetObjectFromPool(string tagname)
+    /* public GameObject GetObjectsFromPool(string tagname)
+     {
+         for(int i=0;i<pool.Count;i++)
+         {   if(pool[i].gameObject.tag == tagname)
+             {
+                 return pool[i].gameObject;
+                // pool[i].gameObject.SetActive(true);
+             }
+
+         }
+     }*/
+    public GameObject GetObjectsFromPool(string tagname)
     {
-        for(int i=0;i<pool.Count;i++)
+        foreach (GameObject item in pool)
         {
-            if(pool[i].gameObject.tag=="astroid")
+            if (item.gameObject.tag == tagname && !item.activeInHierarchy)
             {
-                return pool[i];
+                return item;
+                // pool[i].gameObject.SetActive(true);
             }
+
         }
         return null;
+
     }
 }
+
 [System.Serializable]
 public class PoolObject
 {
-    public GameObject gameObjectPrefab;
+    public GameObject prefab;
+    public string name;
     public int amount;
-    
 }
