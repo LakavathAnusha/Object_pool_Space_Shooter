7947c4e5b8fddf968da4f07700cc7491733a33ee
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-04-21 16:16:00 +0530

    when asteroid hit spaceship then health is decremented

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,24 +4,63 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public GameObject bullet;
-    public Vector3 offset;
+    public int playerSpeed;
+    int playerHealth = 10;
+    int maxHealth = 10;
+    // public GameObject bulletPrefab;
+    public Vector3 offSet;
+    public bool isGameOver = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movement;
-        movement = Input.GetAxis("Horizontal") * speed;
-        transform.Translate(0,-movement*Time.deltaTime, 0);
-        if(Input.GetKeyDown(KeyCode.Space))
+      
+            float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+            transform.Translate(inputY * playerSpeed * Time.deltaTime,inputX * playerSpeed * Time.deltaTime, 0f);
+            //Clamp player position within gameWindow
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                GameObject tempBullet = PoolScript.instance.GetObjectsFromPool("Bullet");
+            if (tempBullet != null)
+            {
+                tempBullet.transform.position = this.transform.position + offSet;
+                // transform.Translate(0f, 0f, 1f);
+                tempBullet.SetActive(true);
+            }
+                //Instantiate(bulletPrefab, transform.position + offSet, Quaternion.identity);
+            }
+        
+       /* if (playerHealth == 0)
         {
-            Instantiate(bullet, transform.position + offset, Quaternion.Euler(0,0,90));
+            Destroy(gameObject);
+            isGameOver = true;
+            print("GameOver");
+
+        }*/
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "asteroid")
+        {
+            playerHealth--;
+            collision.gameObject.SetActive(false);
+            print("player Health Dec:" + playerHealth);
+
         }
+        if (collision.gameObject.tag == "Health" && playerHealth < maxHealth)
+        {
+            playerHealth = Mathf.Clamp(playerHealth + 1, 0, maxHealth);
+            collision.gameObject.SetActive(false);
+            print("player Health inc:" + playerHealth);
+        }
+
     }
 }
